13191f8a90f325ed47c25db0ff3961086bd1f18d
venom-rose <debjeet.kundu@rediffmail.com>
2026-08-09 05:16:48 +0530

    go

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://debjeet-kundu:debjeet%4014072022@leaseify.wnloexf.mongodb.net/?appName=Leaseify');
 
     console.log(`[MongoDB] Connected successfully: ${conn.connection.host}/${conn.connection.name}`);
     return conn;
